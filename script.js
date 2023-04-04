08765d2ee8af9c0906ed759e9a76f279a058dc48
xsnewtons <xsnewtons@gmail.com>
2023-04-04 05:06:03 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("hello there")
+hamburger =document.querySelector(".hamburger");
+        hamburger.onclick=function(){
+            navBar =document.querySelector(".navbar");
+            navBar.classList.toggle("active");   
+        }
+        
